adcfbd725b56a69e527010e7c5a5ac93efbacba1
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-04-18 22:31:32 +0530

    seting fix

--- expense_tracker_app/settings.py
+++ expense_tracker_app/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "statement" / "static" / "include",
-    BASE_DIR / "statement" / "images",
+    BASE_DIR / "static" / "images",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
