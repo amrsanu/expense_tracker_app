97b46e76d1051ace572924aff019a772f3181558
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-04-20 00:21:05 +0530

    filters

--- expense_tracker_app/settings.py
+++ expense_tracker_app/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "statement",
-    "bootstrap4",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- staticfiles/routines/upload_file.js
+++
@@ -1,8 +0,0 @@
-// Function to select/deselect all checkboxes
-function toggle(source) {
-    var checkboxes = document.getElementsByName('file');
-    for(var i=0, n=checkboxes.length;i<n;i++) {
-      checkboxes[i].checked = source.checked;
-    }
-  }
-  
