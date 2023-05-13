a94953ccc56133ca4a585d53aea471556e51e6e4
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 00:04:01 +0530

    fic11

--- expense_tracker_app/settings.py
+++ expense_tracker_app/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
     BASE_DIR / "static",
     BASE_DIR / "statement" / "static",
 ]
