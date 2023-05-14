69ac42b32864140f828b7dc8a093c656043bc309
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 08:02:52 +0530

    re

--- expense_tracker_app/settings.py
+++ expense_tracker_app/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-o&=8^f)a=%-@j^vhn*s^hw547absy#5(qw$i13j*pw^as$4k)b"
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "*"]
 
