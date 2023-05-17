48deb58026590e3bc94c4906bba6c1d8d6e6b79a
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-17 07:46:52 +0530

    setting change

--- expense_tracker_app/settings.py
+++ expense_tracker_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o&=8^f)a=%-@j^vhn*s^hw547absy#5(qw$i13j*pw^as$4k)b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "*"]
 
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/statement/static"
+
+MEDIA_ROOT = BASE_DIR / "statement" / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
